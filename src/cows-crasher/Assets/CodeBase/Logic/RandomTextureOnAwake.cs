02e8825cfc21b8853c43b2a5ca251443b2393083
scrawach <scrawach@gmail.com>
2023-01-13 20:58:49 +0300

    uploaded new cow model, few cow skins and select random skin on awake

---
+++ src/cows-crasher/Assets/CodeBase/Logic/RandomTextureOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CodeBase.Logic
+{
+    public class RandomTextureOnAwake : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _mesh;
+        [SerializeField] private Texture[] _textures;
+        
+        private static readonly int Texture = Shader.PropertyToID("_MainTex");
+
+        private void Awake()
+        {
+            var randomIndex = Random.Range(0, _textures.Length);
+            _mesh.material.SetTexture(Texture, _textures[randomIndex]);
+        }
+    }
+}
