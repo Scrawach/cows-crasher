f23855e303ef071b61f878a7f16830cfc1ca6ceb
scrawach <scrawach@gmail.com>
2023-01-18 14:53:25 +0300

    moved logic with select patrol goal to patrol route

--- src/cows-crasher/Assets/CodeBase/AI/Components/PatrolRoute.cs
+++ src/cows-crasher/Assets/CodeBase/AI/Components/PatrolRoute.cs
@@ -5,5 +5,17 @@ namespace CodeBase.AI.Components
     public class PatrolRoute : MonoBehaviour
     {
         public Transform[] Points;
+
+        private int _goalIndex = -1;
+
+        public Vector3 NextGoal()
+        {
+            _goalIndex++;
+
+            if (_goalIndex >= Points.Length)
+                _goalIndex = 0;
+
+            return Points[_goalIndex].position;
+        }
     }
 }
--- src/cows-crasher/Assets/CodeBase/AI/Cow/States/CowPatrolState.cs
+++ src/cows-crasher/Assets/CodeBase/AI/Cow/States/CowPatrolState.cs
@@ -1,9 +1,7 @@
 using CodeBase.AI.Components;
 using CodeBase.AI.Cow.States.Abstract;
-using CodeBase.AI.Cow.Transitions.Abstract;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 namespace CodeBase.AI.Cow.States
 {
@@ -11,33 +9,22 @@ namespace CodeBase.AI.Cow.States
     {
         [SerializeField] private NavMeshAgent _agent;
         [SerializeField] private AgentMoveToPoint _moveToPoint;
-        [SerializeField] private PatrolRoute patrolRoute;
+        [SerializeField] private PatrolRoute _patrolRoute;
         
         private Vector3 _currentGoal;
-        private int _goalIndex = -1;
 
         public bool IsReachedPoint => !_moveToPoint.TargetNotReached();
 
         public override void Enter()
         {
             _agent.enabled = true;
-            _moveToPoint.MoveTo(NextGoal());
+            _moveToPoint.MoveTo(_patrolRoute.NextGoal());
         }
 
         public override void Exit()
         {
             _moveToPoint.ResetMovement();
             _agent.enabled = false;
-        }
-
-        private Vector3 NextGoal()
-        {
-            _goalIndex++;
-
-            if (_goalIndex >= patrolRoute.Points.Length)
-                _goalIndex = 0;
-
-            return patrolRoute.Points[_goalIndex].position;
         }
     }
 }
