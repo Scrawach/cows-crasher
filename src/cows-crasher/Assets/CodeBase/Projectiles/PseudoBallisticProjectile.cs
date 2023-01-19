adbb6a3fc13600a13a5d54fd65c3529e9fb554ac
scrawach <scrawach@gmail.com>
2023-01-19 12:10:26 +0300

    moved assets to suitable folder, a little bit refactoring for pseudoballistic projectile

--- src/cows-crasher/Assets/CodeBase/Projectiles/PseudoBallisticProjectile.cs
+++ src/cows-crasher/Assets/CodeBase/Projectiles/PseudoBallisticProjectile.cs
@@ -6,43 +6,45 @@ namespace CodeBase.Projectiles
     public class PseudoBallisticProjectile : Projectile
     {
         [SerializeField] private float _speed;
-        [SerializeField] private AnimationCurve _height;
         [SerializeField] private float _heightOffset;
+        [SerializeField] private AnimationCurve _climbCurve;
         [SerializeField] private Timer _climbTimer;
         
         private Vector3 _direction;
-        private Vector3 _target;
         private float _startHeight;
+        private float _targetHeight;
         
         public override void Construct(Vector3 target)
         {
-            _target = target;
-            _direction = target - transform.position;
-            _direction.y = 0;
-            _direction.Normalize();
             _startHeight = transform.position.y;
+            _targetHeight = target.y + _heightOffset;
+            _direction = DirectionWithoutHeightTo(target);
             _climbTimer.Play();
         }
 
         private void Update()
         {
-            var timeStep = Time.deltaTime * _speed;
-            var desiredPosition = transform.position + _direction * timeStep;
+            var step = Time.deltaTime * _speed;
+            var desiredPosition = transform.position + _direction * step;
             
             if (_climbTimer.IsPlaying)
             {
-                desiredPosition.y = CalculateHeight(_climbTimer.Progress);
+                desiredPosition.y = CalculateHeight(_startHeight, _targetHeight, _climbTimer.Progress);
                 transform.rotation = Quaternion.LookRotation((desiredPosition - transform.position).normalized);
-            }
-            else
-            {
-                desiredPosition.y = _target.y + _heightOffset;
             }
 
             transform.position = desiredPosition;
         }
 
-        private float CalculateHeight(float time) =>
-            Mathf.Lerp(_startHeight, _target.y + _heightOffset, _height.Evaluate(time));
+        private float CalculateHeight(float start, float target, float time) =>
+            Mathf.Lerp(start, target, _climbCurve.Evaluate(time));
+
+        private Vector3 DirectionWithoutHeightTo(Vector3 target)
+        {
+            var position = transform.position;
+            var direction = target - position;
+            direction.y = 0;
+            return direction.normalized;
+        }
     }
 }
